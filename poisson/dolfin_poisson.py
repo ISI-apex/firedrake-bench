9efc99ae5151f24f991f0de85be3da8450e26416
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-21 15:55:27 +0100

    Convert Poisson benchmarks to use pybench

--- poisson/dolfin_poisson.py
+++ poisson/dolfin_poisson.py
@@ -1,51 +1,53 @@
-from time import clock
+from pybench import Benchmark
 from dolfin import *
 
 make_mesh = {2: lambda x: UnitSquareMesh(x, x),
              3: lambda x: UnitCubeMesh(x, x, x)}
 
 
-def poisson(size, degree=1, dim=2):
-    t_ = clock()
-    mesh = make_mesh[dim](size)
-    mesh.init()
-    print dim, size, degree, 'mesh:', clock() - t_
-    t_ = clock()
-    V = FunctionSpace(mesh, "Lagrange", 1)
+class DolfinPoisson(Benchmark):
 
-    # Define Dirichlet boundary (x = 0 or x = 1)
-    def boundary(x):
-        return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
+    params = {'degree': range(1, 4),
+              'size': [2**x for x in range(4, 7)],
+              'dim': [3]}
+    method = 'poisson'
 
-    # Define boundary condition
-    u0 = Constant(0.0)
-    bc = DirichletBC(V, u0, boundary)
+    def poisson(self, size, degree=1, dim=2):
+        with self.timed_region('mesh'):
+            mesh = make_mesh[dim](size)
+            mesh.init()
+        with self.timed_region('setup'):
+            V = FunctionSpace(mesh, "Lagrange", 1)
 
-    # Define variational problem
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
-    g = Expression("sin(5*x[0])")
-    a = inner(grad(u), grad(v))*dx
-    L = f*v*dx + g*v*ds
+            # Define Dirichlet boundary (x = 0 or x = 1)
+            def boundary(x):
+                return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
 
-    # Compute solution
-    u = Function(V)
-    print dim, size, degree, 'setup:', clock() - t_
-    t_ = clock()
-    A = assemble(a)
-    bc.apply(A)
-    print dim, size, degree, 'matrix:', clock() - t_
-    t_ = clock()
-    b = assemble(L)
-    bc.apply(b)
-    print dim, size, degree, 'rhs:', clock() - t_
-    t_ = clock()
-    solve(A, u.vector(), b, "cg", "default")
-    print dim, size, degree, 'solve:', clock() - t_
+            # Define boundary condition
+            u0 = Constant(0.0)
+            bc = DirichletBC(V, u0, boundary)
+
+            # Define variational problem
+            u = TrialFunction(V)
+            v = TestFunction(V)
+            f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
+            g = Expression("sin(5*x[0])")
+            a = inner(grad(u), grad(v))*dx
+            L = f*v*dx + g*v*ds
+
+            # Compute solution
+            u = Function(V)
+        with self.timed_region('matrix assembly'):
+            A = assemble(a)
+            bc.apply(A)
+        with self.timed_region('rhs assembly'):
+            b = assemble(L)
+            bc.apply(b)
+        with self.timed_region('solve'):
+            solve(A, u.vector(), b, "cg", "default")
 
 if __name__ == '__main__':
     set_log_active(False)
-    for degree in range(1, 4):
-        for size in range(4, 7):
-            poisson(2**size, degree=degree, dim=3)
+    b = DolfinPoisson()
+    b.run()
+    b.plot(xaxis='size')
--- poisson/firedrake_poisson.py
+++ poisson/firedrake_poisson.py
@@ -1,45 +1,47 @@
-from time import clock
+from pybench import Benchmark
 from firedrake import *
 
 make_mesh = {2: lambda x: UnitSquareMesh(x, x),
              3: lambda x: UnitCubeMesh(x, x, x)}
 
 
-def poisson(size, degree=1, dim=2):
-    t_ = clock()
-    mesh = make_mesh[dim](size)
-    print dim, size, degree, 'mesh:', clock() - t_
-    t_ = clock()
-    V = FunctionSpace(mesh, "Lagrange", 1)
+class FiredrakePoisson(Benchmark):
 
-    # Define boundary condition
-    bc = DirichletBC(V, 0.0, [3, 4])
+    params = {'degree': range(1, 4),
+              'size': [2**x for x in range(4, 7)],
+              'dim': [3]}
+    method = 'poisson'
 
-    # Define variational problem
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    f = Function(V).interpolate(Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)"))
-    g = Function(V).interpolate(Expression("sin(5*x[0])"))
-    a = inner(grad(u), grad(v))*dx
-    L = f*v*dx + g*v*ds
+    def poisson(self, size, degree=1, dim=2):
+        with self.timed_region('mesh'):
+            mesh = make_mesh[dim](size)
+        with self.timed_region('setup'):
+            V = FunctionSpace(mesh, "Lagrange", 1)
 
-    # Compute solution
-    u = Function(V)
-    print dim, size, degree, 'setup:', clock() - t_
-    t_ = clock()
-    A = assemble(a, bcs=bc)
-    print dim, size, degree, 'matrix:', clock() - t_
-    t_ = clock()
-    b = assemble(L)
-    bc.apply(b)
-    print dim, size, degree, 'rhs:', clock() - t_
-    t_ = clock()
-    solve(A, u, b, solver_parameters={"ksp_type": "cg"})
-    u.dat.data
-    print dim, size, degree, 'solve:', clock() - t_
+            # Define boundary condition
+            bc = DirichletBC(V, 0.0, [3, 4])
+
+            # Define variational problem
+            u = TrialFunction(V)
+            v = TestFunction(V)
+            f = Function(V).interpolate(Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)"))
+            g = Function(V).interpolate(Expression("sin(5*x[0])"))
+            a = inner(grad(u), grad(v))*dx
+            L = f*v*dx + g*v*ds
+
+            # Compute solution
+            u = Function(V)
+        with self.timed_region('matrix assembly'):
+            A = assemble(a, bcs=bc)
+        with self.timed_region('rhs assembly'):
+            b = assemble(L)
+            bc.apply(b)
+        with self.timed_region('solve'):
+            solve(A, u, b, solver_parameters={"ksp_type": "cg"})
+            u.dat.data
 
 if __name__ == '__main__':
     op2.init(log_level='WARNING')
-    for degree in range(1, 4):
-        for size in range(4, 7):
-            poisson(2**size, degree=degree, dim=3)
+    b = FiredrakePoisson()
+    b.run()
+    b.plot(xaxis='size')
