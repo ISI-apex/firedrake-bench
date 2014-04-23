8a5962b060fe8c1b00fecd91c8814ea8ff22e8c7
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-23 15:08:18 +0100

    Update params for Poisson benchmark
    Create a series of meshes that roughly double in number of DOFs.

--- poisson/poisson.py
+++ poisson/poisson.py
@@ -1,11 +1,14 @@
 from pybench import Benchmark
 
+dim = 3
+# Create a series of meshes that roughly double in number of DOFs
+sizes = [int((1e4*2**x)**(1./dim)) + 1 for x in range(5)]
 
 class Poisson(Benchmark):
 
-    params = {'degree': range(1, 4),
-              'size': [2**x for x in range(4, 7)],
-              'dim': [3]}
+    params = [('dim', [dim]),
+              ('degree', range(1, 4)),
+              ('size', sizes)]
     method = 'poisson'
     profilegraph = {'format': 'svg,pdf',
                     'node_threshold': 2.0}
