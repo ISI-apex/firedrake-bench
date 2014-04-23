4321f8639a981099c2e4fc7daebdcd7ea07f4bda
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-23 16:16:12 +0100

    Script plots for Poisson benchmarks
    Combine a series of a parallel run over several processors and plot
    the sequential result over mesh size and parallel scaling for each
    parameter combination.

--- poisson/poisson.py
+++ poisson/poisson.py
@@ -16,6 +16,15 @@ class Poisson(Benchmark):
                     'node_threshold': 2.0}
 
 if __name__ == '__main__':
-    Poisson().main(combine={'FiredrakePoisson': 'Firedrake',
-                            'DolfinPoisson': 'DOLFIN'},
-                   plot=['size'])
+    b = Poisson(name='DolfinPoissonParallel')
+    b.combine_series([('np', [1, 2, 4, 6])], filename='DolfinPoisson')
+    b.save()
+    b = Poisson(name='FiredrakePoissonParallel')
+    b.combine_series([('np', [1, 2, 4, 6])], filename='FiredrakePoisson')
+    b.save()
+    b = Poisson()
+    b.combine({'FiredrakePoisson_np1': 'Firedrake', 'DolfinPoisson_np1': 'DOLFIN'})
+    b.plot(xaxis='size')
+    b = Poisson(name='PoissonParallel')
+    b.combine({'FiredrakePoissonParallel': 'Firedrake', 'DolfinPoissonParallel': 'DOLFIN'})
+    b.plot(xaxis='np')
