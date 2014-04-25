92bc9580fd9eed9229c6610673a8ff2c2a38b3d3
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-25 10:56:35 +0100

    Convert Navier-Stokes benchmarks to pybench

--- navier_stokes/dolfin_navier_stokes.py
+++ navier_stokes/dolfin_navier_stokes.py
@@ -1,144 +1,189 @@
-from time import clock
+from navier_stokes import NavierStokes
 from dolfin import *
 
 # Don't reorder the mesh
 parameters["reorder_dofs_serial"] = False
 
 
-def navier_stokes(x, T=3, preassemble=True, save=False):
-    # Load mesh from file
-    t_ = clock()
-    mesh = Mesh("lshape_%s.xml.gz" % scale)
-    mesh.init()
-    print scale, 'mesh:', clock() - t_
-    t_ = clock()
+class DolfinNavierStokes(NavierStokes):
 
-    # Define function spaces (P2-P1)
-    V = VectorFunctionSpace(mesh, "Lagrange", 1)
-    Q = FunctionSpace(mesh, "Lagrange", 1)
+    series = {'np': MPI.size(mpi_comm_world())}
+    plotstyle = {'total': {'color': 'black',
+                           'marker': '*',
+                           'linestyle': '--'},
+                 'mesh': {'color': 'blue',
+                          'marker': '+',
+                          'linestyle': '--'},
+                 'setup': {'color': 'green',
+                           'marker': 'x',
+                           'linestyle': '--'},
+                 'matrix assembly': {'color': 'blue',
+                                     'marker': 'p',
+                                     'linestyle': '--'},
+                 'timestepping': {'color': 'yellow',
+                                  'marker': 'o',
+                                  'linestyle': '--'},
+                 'tentative velocity RHS': {'color': 'magenta',
+                                            'marker': '^',
+                                            'linestyle': '--'},
+                 'tentative velocity solve': {'color': 'magenta',
+                                              'marker': 'v',
+                                              'linestyle': '--'},
+                 'pressure correction RHS': {'color': 'red',
+                                             'marker': 's',
+                                             'linestyle': '--'},
+                 'pressure correction solve': {'color': 'red',
+                                               'marker': 'D',
+                                               'linestyle': '--'},
+                 'velocity correction RHS': {'color': 'cyan',
+                                             'marker': '>',
+                                             'linestyle': '--'},
+                 'velocity correction solve': {'color': 'cyan',
+                                               'marker': '<',
+                                               'linestyle': '--'}}
 
-    # Define trial and test functions
-    u = TrialFunction(V)
-    p = TrialFunction(Q)
-    v = TestFunction(V)
-    q = TestFunction(Q)
+    def navier_stokes(self, scale=1, T=1, preassemble=True, save=False):
+        with self.timed_region('mesh'):
+            # Load mesh from file
+            mesh = Mesh("lshape_%s.xml.gz" % scale)
+            mesh.init()
 
-    # Set parameter values
-    dt = 0.01
-    nu = 0.01
+        with self.timed_region('setup'):
+            # Define function spaces (P2-P1)
+            V = VectorFunctionSpace(mesh, "Lagrange", 2)
+            Q = FunctionSpace(mesh, "Lagrange", 1)
 
-    # Define time-dependent pressure boundary condition
-    p_in = Expression("sin(3.0*t)", t=0.0)
+            # Define trial and test functions
+            u = TrialFunction(V)
+            p = TrialFunction(Q)
+            v = TestFunction(V)
+            q = TestFunction(Q)
 
-    # Define boundary conditions
-    noslip = DirichletBC(V, (0, 0),
-                         "on_boundary && \
-                          (x[0] < DOLFIN_EPS | x[1] < DOLFIN_EPS | \
-                          (x[0] > 0.5 - DOLFIN_EPS && x[1] > 0.5 - DOLFIN_EPS))")
-    inflow = DirichletBC(Q, p_in, "x[1] > 1.0 - DOLFIN_EPS")
-    outflow = DirichletBC(Q, 0, "x[0] > 1.0 - DOLFIN_EPS")
-    bcu = [noslip]
-    bcp = [inflow, outflow]
+            # Set parameter values
+            dt = 0.01
+            nu = 0.01
 
-    # Create functions
-    u0 = Function(V)
-    u1 = Function(V)
-    p1 = Function(Q)
+            # Define time-dependent pressure boundary condition
+            p_in = Expression("sin(3.0*t)", t=0.0)
 
-    # Define coefficients
-    k = Constant(dt)
-    f = Constant((0, 0))
+            # Define boundary conditions
+            noslip = DirichletBC(V, (0, 0),
+                                 "on_boundary && \
+                                  (x[0] < DOLFIN_EPS | x[1] < DOLFIN_EPS | \
+                                  (x[0] > 0.5 - DOLFIN_EPS && x[1] > 0.5 - DOLFIN_EPS))")
+            inflow = DirichletBC(Q, p_in, "x[1] > 1.0 - DOLFIN_EPS")
+            outflow = DirichletBC(Q, 0, "x[0] > 1.0 - DOLFIN_EPS")
+            bcu = [noslip]
+            bcp = [inflow, outflow]
 
-    # Tentative velocity step
-    F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
-        nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
-    a1 = lhs(F1)
-    L1 = rhs(F1)
+            # Create functions
+            u0 = Function(V)
+            u1 = Function(V)
+            p1 = Function(Q)
 
-    # Pressure update
-    a2 = inner(grad(p), grad(q))*dx
-    L2 = -(1/k)*div(u1)*q*dx
+            # Define coefficients
+            k = Constant(dt)
+            f = Constant((0, 0))
 
-    # Velocity update
-    a3 = inner(u, v)*dx
-    L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
-    print scale, 'setup:', clock() - t_
-    t_ = clock()
+            # Tentative velocity step
+            F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
+                nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
+            a1 = lhs(F1)
+            L1 = rhs(F1)
 
-    if preassemble:
-        # Assemble matrices
-        A1 = assemble(a1)
-        A2 = assemble(a2)
-        A3 = assemble(a3)
+            # Pressure update
+            a2 = inner(grad(p), grad(q))*dx
+            L2 = -(1/k)*div(u1)*q*dx
 
-    print scale, 'matrix assembly:', clock() - t_
-    t_ = clock()
+            # Velocity update
+            a3 = inner(u, v)*dx
+            L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
 
-    if save:
-        # Create files for storing solution
-        ufile = File("results/dolfin_velocity.pvd")
-        pfile = File("results/dolfin_pressure.pvd")
-
-    # Time-stepping
-    t = dt
-    while t < T + DOLFIN_EPS:
-        print "t =", t
-        print 'u0', norm(u0)
-
-        # Update pressure boundary condition
-        p_in.t = t
-
-        # Compute tentative velocity step
-        begin("Computing tentative velocity")
         if preassemble:
-            b1 = assemble(L1)
-            [bc.apply(A1, b1) for bc in bcu]
-            solve(A1, u1.vector(), b1, "gmres", "ilu")
-        else:
-            solve(a1 == L1, u1, bcs=bcu,
-                  solver_parameters={"linear_solver": "gmres",
-                                     "preconditioner": "ilu"})
-        print 'u1', norm(u1)
-        end()
-
-        # Pressure correction
-        begin("Computing pressure correction")
-        if preassemble:
-            b2 = assemble(L2)
-            [bc.apply(A2, b2) for bc in bcp]
-            solve(A2, p1.vector(), b2, "cg", "default")
-        else:
-            solve(a2 == L2, p1, bcs=bcp,
-                  solver_parameters={"linear_solver": "cg",
-                                     "preconditioner": "default"})
-        print 'p1', norm(p1)
-        end()
-
-        # Velocity correction
-        begin("Computing velocity correction")
-        if preassemble:
-            b3 = assemble(L3)
-            [bc.apply(A3, b3) for bc in bcu]
-            solve(A3, u1.vector(), b3, "gmres", "ilu")
-        else:
-            solve(a3 == L3, u1, bcs=bcu,
-                  solver_parameters={"linear_solver": "gmres",
-                                     "preconditioner": "ilu"})
-        print 'u1', norm(u1)
-        end()
+            with self.timed_region('matrix assembly'):
+                # Assemble matrices
+                A1 = assemble(a1)
+                A2 = assemble(a2)
+                A3 = assemble(a3)
 
         if save:
-            # Save to file
-            ufile << u1
-            pfile << p1
+            # Create files for storing solution
+            ufile = File("vtk/dolfin_velocity.pvd")
+            pfile = File("vtk/dolfin_pressure.pvd")
 
-        # Move to next time step
-        u0.assign(u1)
-        t += dt
+        with self.timed_region('timestepping'):
+            # Time-stepping
+            t = dt
+            while t < T + DOLFIN_EPS:
+                # Update pressure boundary condition
+                p_in.t = t
 
-    print scale, 'time stepping', clock() - t_
+                # Compute tentative velocity step
+                begin("Computing tentative velocity")
+                if preassemble:
+                    with self.timed_region('tentative velocity RHS'):
+                        b1 = assemble(L1)
+                        [bc.apply(A1, b1) for bc in bcu]
+                    with self.timed_region('tentative velocity solve'):
+                        solve(A1, u1.vector(), b1, "gmres", "ilu")
+                else:
+                    with self.timed_region('tentative velocity solve'):
+                        solve(a1 == L1, u1, bcs=bcu,
+                              solver_parameters={"linear_solver": "gmres",
+                                                 "preconditioner": "ilu"})
+                end()
+
+                # Pressure correction
+                begin("Computing pressure correction")
+                if preassemble:
+                    with self.timed_region('pressure correction RHS'):
+                        b2 = assemble(L2)
+                        [bc.apply(A2, b2) for bc in bcp]
+                    with self.timed_region('pressure correction solve'):
+                        solve(A2, p1.vector(), b2, "cg", "ilu")
+                else:
+                    with self.timed_region('pressure correction solve'):
+                        solve(a2 == L2, p1, bcs=bcp,
+                              solver_parameters={"linear_solver": "cg",
+                                                 "preconditioner": "ilu"})
+                end()
+
+                # Velocity correction
+                begin("Computing velocity correction")
+                if preassemble:
+                    with self.timed_region('velocity correction RHS'):
+                        b3 = assemble(L3)
+                        [bc.apply(A3, b3) for bc in bcu]
+                    with self.timed_region('velocity correction solve'):
+                        solve(A3, u1.vector(), b3, "gmres", "ilu")
+                else:
+                    with self.timed_region('velocity correction solve'):
+                        solve(a3 == L3, u1, bcs=bcu,
+                              solver_parameters={"linear_solver": "gmres",
+                                                 "preconditioner": "ilu"})
+                end()
+
+                if save:
+                    # Save to file
+                    ufile << u1
+                    pfile << p1
+
+                # Move to next time step
+                u0.assign(u1)
+                t += dt
 
 if __name__ == '__main__':
     set_log_active(False)
-    for scale in (2, 1, 0.5):
-        navier_stokes(scale, 1)
+
+    # Benchmark
+    DolfinNavierStokes().main(benchmark=True, save=None)
+
+    # Output VTU files
+    # DolfinNavierStokes().navier_stokes(save=True)
+
+    # Profile
+    # from itertools import product
+    # r1 = ['tentative velocity', 'pressure correction', 'velocity correction']
+    # r2 = ['RHS', 'solve']
+    # regions = ['matrix assembly'] + map(' '.join, product(r1, r2))
+    # DolfinNavierStokes().profile(regions=regions)
--- navier_stokes/firedrake_navier_stokes.py
+++ navier_stokes/firedrake_navier_stokes.py
@@ -1,136 +1,185 @@
-from time import clock
+from navier_stokes import NavierStokes
 from firedrake import *
 
 
-def navier_stokes(scale, T=3, preassemble=True, save=False):
-    # Load mesh from file
-    t_ = clock()
-    mesh = Mesh("lshape_%s.msh" % scale)
-    print scale, 'mesh:', clock() - t_
-    t_ = clock()
+class FiredrakeNavierStokes(NavierStokes):
 
-    # Define function spaces (P2-P1)
-    V = VectorFunctionSpace(mesh, "Lagrange", 2)
-    Q = FunctionSpace(mesh, "Lagrange", 1)
+    series = {'np': op2.MPI.comm.size}
+    plotstyle = {'total': {'color': 'black',
+                           'marker': '*',
+                           'linestyle': '-'},
+                 'mesh': {'color': 'blue',
+                          'marker': '+',
+                          'linestyle': '-'},
+                 'setup': {'color': 'green',
+                           'marker': 'x',
+                           'linestyle': '-'},
+                 'matrix assembly': {'color': 'blue',
+                                     'marker': 'p',
+                                     'linestyle': '-'},
+                 'timestepping': {'color': 'yellow',
+                                  'marker': 'o',
+                                  'linestyle': '-'},
+                 'tentative velocity RHS': {'color': 'magenta',
+                                            'marker': '^',
+                                            'linestyle': '-'},
+                 'tentative velocity solve': {'color': 'magenta',
+                                              'marker': 'v',
+                                              'linestyle': '-'},
+                 'pressure correction RHS': {'color': 'red',
+                                             'marker': 's',
+                                             'linestyle': '-'},
+                 'pressure correction solve': {'color': 'red',
+                                               'marker': 'D',
+                                               'linestyle': '-'},
+                 'velocity correction RHS': {'color': 'cyan',
+                                             'marker': '>',
+                                             'linestyle': '-'},
+                 'velocity correction solve': {'color': 'cyan',
+                                               'marker': '<',
+                                               'linestyle': '-'}}
 
-    # Define trial and test functions
-    u = TrialFunction(V)
-    p = TrialFunction(Q)
-    v = TestFunction(V)
-    q = TestFunction(Q)
+    def navier_stokes(self, scale=1, T=1, preassemble=True, save=False):
+        with self.timed_region('mesh'):
+            # Load mesh from file
+            mesh = Mesh("lshape_%s.msh" % scale)
 
-    # Set parameter values
-    dt = 0.01
-    nu = 0.01
+        with self.timed_region('setup'):
+            # Define function spaces (P2-P1)
+            V = VectorFunctionSpace(mesh, "Lagrange", 2)
+            Q = FunctionSpace(mesh, "Lagrange", 1)
 
-    # Define time-dependent pressure boundary condition
-    p_in = Constant(0.0)
+            # Define trial and test functions
+            u = TrialFunction(V)
+            p = TrialFunction(Q)
+            v = TestFunction(V)
+            q = TestFunction(Q)
 
-    # Define boundary conditions
-    noslip = DirichletBC(V, Constant((0.0, 0.0)), (1, 3, 4, 6))
-    inflow = DirichletBC(Q, p_in, 5)
-    outflow = DirichletBC(Q, 0, 2)
-    bcu = [noslip]
-    bcp = [inflow, outflow]
+            # Set parameter values
+            dt = 0.01
+            nu = 0.01
 
-    # Create functions
-    u0 = Function(V)
-    u1 = Function(V)
-    p1 = Function(Q)
+            # Define time-dependent pressure boundary condition
+            p_in = Constant(0.0)
 
-    # Define coefficients
-    k = Constant(dt)
-    f = Constant((0, 0))
+            # Define boundary conditions
+            noslip = DirichletBC(V, Constant((0.0, 0.0)), (1, 3, 4, 6))
+            inflow = DirichletBC(Q, p_in, 5)
+            outflow = DirichletBC(Q, 0, 2)
+            bcu = [noslip]
+            bcp = [inflow, outflow]
 
-    # Tentative velocity step
-    F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
-        nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
-    a1 = lhs(F1)
-    L1 = rhs(F1)
+            # Create functions
+            u0 = Function(V)
+            u1 = Function(V)
+            p1 = Function(Q)
 
-    # Pressure update
-    a2 = inner(grad(p), grad(q))*dx
-    L2 = -(1/k)*div(u1)*q*dx
+            # Define coefficients
+            k = Constant(dt)
+            f = Constant((0, 0))
 
-    # Velocity update
-    a3 = inner(u, v)*dx
-    L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
-    print scale, 'setup:', clock() - t_
-    t_ = clock()
+            # Tentative velocity step
+            F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
+                nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
+            a1 = lhs(F1)
+            L1 = rhs(F1)
 
-    if preassemble:
-        # Assemble matrices
-        A1 = assemble(a1)
-        A2 = assemble(a2)
-        A3 = assemble(a3)
+            # Pressure update
+            a2 = inner(grad(p), grad(q))*dx
+            L2 = -(1/k)*div(u1)*q*dx
 
-    print scale, 'matrix assembly:', clock() - t_
-    t_ = clock()
+            # Velocity update
+            a3 = inner(u, v)*dx
+            L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
 
-    if save:
-        # Create files for storing solution
-        ufile = File("results/firedrake_velocity.pvd")
-        pfile = File("results/firedrake_pressure.pvd")
-
-    vparams = {'ksp_type': 'gmres',
-               'pc_type': 'ilu',
-               'ksp_rtol': 1e-6,
-               'ksp_atol': 1e-15}
-    pparams = {'ksp_type': 'cg',
-               'pc_type': 'ilu',
-               'ksp_rtol': 1e-6,
-               'ksp_atol': 1e-15}
-    # Time-stepping
-    t = dt
-    while t < T + 1e-14:
-        print "t =", t
-        print 'u0', norm(u0)
-
-        # Update pressure boundary condition
-        p_in.assign(sin(3.0*t))
-
-        # Compute tentative velocity step
-        info("Computing tentative velocity")
         if preassemble:
-            b1 = assemble(L1)
-            [bc.apply(A1, b1) for bc in bcu]
-            solve(A1, u1, b1, solver_parameters=vparams)
-        else:
-            solve(a1 == L1, u1, bcs=bcu, solver_parameters=vparams)
-        print 'u1', norm(u1)
-
-        # Pressure correction
-        info("Computing pressure correction")
-        if preassemble:
-            b2 = assemble(L2)
-            [bc.apply(A2, b2) for bc in bcp]
-            solve(A2, p1, b2, solver_parameters=pparams)
-        else:
-            solve(a2 == L2, p1, bcs=bcp, solver_parameters=pparams)
-        print 'p1', norm(p1)
-
-        # Velocity correction
-        info("Computing velocity correction")
-        if preassemble:
-            b3 = assemble(L3)
-            [bc.apply(A3, b3) for bc in bcu]
-            solve(A3, u1, b3, solver_parameters=vparams)
-        else:
-            solve(a3 == L3, u1, bcs=bcu, solver_parameters=vparams)
-        print 'u1', norm(u1)
+            with self.timed_region('matrix assembly'):
+                # Assemble matrices
+                A1 = assemble(a1)
+                A2 = assemble(a2)
+                A3 = assemble(a3)
 
         if save:
-            # Save to file
-            ufile << u1
-            pfile << p1
+            # Create files for storing solution
+            ufile = File("vtk/firedrake_velocity.pvd")
+            pfile = File("vtk/firedrake_pressure.pvd")
 
-        # Move to next time step
-        u0.assign(u1)
-        t += dt
+        vparams = {'ksp_type': 'gmres',
+                   'pc_type': 'ilu',
+                   'ksp_rtol': 1e-6,
+                   'ksp_atol': 1e-15}
+        pparams = {'ksp_type': 'cg',
+                   'pc_type': 'ilu',
+                   'ksp_rtol': 1e-6,
+                   'ksp_atol': 1e-15}
 
-    print scale, 'time stepping', clock() - t_
+        with self.timed_region('timestepping'):
+            # Time-stepping
+            t = dt
+            while t < T + 1e-14:
+                # Update pressure boundary condition
+                p_in.assign(sin(3.0*t))
+
+                # Compute tentative velocity step
+                info("Computing tentative velocity")
+                if preassemble:
+                    with self.timed_region('tentative velocity RHS'):
+                        b1 = assemble(L1)
+                        [bc.apply(A1, b1) for bc in bcu]
+                        b1.dat.data
+                    with self.timed_region('tentative velocity solve'):
+                        solve(A1, u1, b1, solver_parameters=vparams)
+                else:
+                    with self.timed_region('tentative velocity solve'):
+                        solve(a1 == L1, u1, bcs=bcu, solver_parameters=vparams)
+
+                # Pressure correction
+                info("Computing pressure correction")
+                if preassemble:
+                    with self.timed_region('pressure correction RHS'):
+                        b2 = assemble(L2)
+                        [bc.apply(A2, b2) for bc in bcp]
+                        b2.dat.data
+                    with self.timed_region('pressure correction solve'):
+                        solve(A2, p1, b2, solver_parameters=pparams)
+                else:
+                    with self.timed_region('pressure correction solve'):
+                        solve(a2 == L2, p1, bcs=bcp, solver_parameters=pparams)
+
+                # Velocity correction
+                info("Computing velocity correction")
+                if preassemble:
+                    with self.timed_region('velocity correction RHS'):
+                        b3 = assemble(L3)
+                        [bc.apply(A3, b3) for bc in bcu]
+                        b3.dat.data
+                    with self.timed_region('velocity correction solve'):
+                        solve(A3, u1, b3, solver_parameters=vparams)
+                else:
+                    with self.timed_region('velocity correction solve'):
+                        solve(a3 == L3, u1, bcs=bcu, solver_parameters=vparams)
+
+                if save:
+                    # Save to file
+                    ufile << u1
+                    pfile << p1
+
+                # Move to next time step
+                u0.assign(u1)
+                t += dt
 
 if __name__ == '__main__':
     op2.init(log_level='WARNING')
-    for scale in (2, 1, 0.5):
-        navier_stokes(scale, 1)
+
+    # Benchmark
+    FiredrakeNavierStokes().main(benchmark=True, save=None)
+
+    # Output VTU files
+    # FiredrakeNavierStokes().navier_stokes(save=True)
+
+    # Profile
+    # from itertools import product
+    # r1 = ['tentative velocity', 'pressure correction', 'velocity correction']
+    # r2 = ['RHS', 'solve']
+    # regions = ['matrix assembly'] + map(' '.join, product(r1, r2))
+    # FiredrakeNavierStokes().profile(regions=regions)
