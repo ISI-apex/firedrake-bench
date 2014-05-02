27c63c2094dd9b3f2891b9e0ed822fde47d90a74
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-02 16:30:03 +0100

    Navier-Stokes: always preassemble in benchmark

--- navier_stokes/navier_stokes.py
+++ navier_stokes/navier_stokes.py
@@ -3,10 +3,9 @@ from pybench import Benchmark
 
 class NavierStokes(Benchmark):
 
-    params = [('scale', [0.8, 0.56, 0.4, 0.28, 0.2]),
-              ('preassemble', [True, False])]
-    meta = {'cells': [30906, 63432, 124390, 253050, 496156, 1013484, 1981076],
-            'dofs': [15451, 31714, 62193, 126523, 248076, 506740, 990536]}
+    params = [('scale', [0.8, 0.56, 0.4, 0.28, 0.2])]
+    meta = {'cells': [30906, 63432, 124390, 253050, 496156],
+            'dofs': [15451, 31714, 62193, 126523, 248076]}
     method = 'navier_stokes'
     profilegraph = {'format': 'svg,pdf',
                     'node_threshold': 2.0}
