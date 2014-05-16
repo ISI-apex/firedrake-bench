15476d5452ef6e5298d9e0ad08de35ea6e9644bb
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-16 18:45:03 +0100

    Poisson: only go up to 500k cell mesh

--- poisson/poisson.py
+++ poisson/poisson.py
@@ -3,7 +3,7 @@ from itertools import product
 
 dim = 3
 # Create a series of meshes that roughly double in number of DOFs
-sizes = [int((1e4*2**x)**(1./dim)) + 1 for x in range(5)]
+sizes = [int((1e4*2**x)**(1./dim)) + 1 for x in range(4)]
 r0 = ['DOLFIN', 'Firedrake']
 r1 = ['matrix assembly', 'rhs assembly', 'solve']
 
