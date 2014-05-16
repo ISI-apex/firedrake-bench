3723ba21da6331468e5f21ef7d044e7ff8f1cf9b
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-17 00:20:01 +0100

    Matrix assembly: parallel plots for np given as arguments

--- matrix_assembly/matrix_assembly.py
+++ matrix_assembly/matrix_assembly.py
@@ -4,7 +4,6 @@ dim = 3
 # Create a series of meshes that roughly double in number of DOFs
 sizes = [int((1e4*2**x)**(1./dim)) + 1 for x in range(4)]
 r1 = ['assembly', 'reassembly', 'assembly bcs', 'reassembly bcs']
-np = [1, 2, 3]
 
 
 class MatrixAssembly(Benchmark):
@@ -21,22 +20,25 @@ class MatrixAssembly(Benchmark):
     profileregions = r1
 
 if __name__ == '__main__':
+    import sys
     from itertools import product
     r0 = ['DOLFIN', 'Firedrake']
     regions = map(' '.join, product(r0, r1))
-    b = MatrixAssembly(name='DolfinMatrixAssemblyParallel')
-    b.combine_series([('np', np)], filename='DolfinMatrixAssembly')
-    b.save()
-    b = MatrixAssembly(name='FiredrakeMatrixAssemblyParallel')
-    b.combine_series([('np', np)], filename='FiredrakeMatrixAssembly')
-    b.save()
     b = MatrixAssembly()
     b.combine({'FiredrakeMatrixAssembly_np1': 'Firedrake',
                'DolfinMatrixAssembly_np1': 'DOLFIN'})
     b.plot(xaxis='size', regions=regions, xlabel='mesh size (cells)',
            xvalues=b.meta['cells'])
     b.plot(xaxis='degree', regions=regions, xlabel='Polynomial degree')
-    b = MatrixAssembly(name='MatrixAssemblyParallel')
-    b.combine({'FiredrakeMatrixAssemblyParallel': 'Firedrake',
-               'DolfinMatrixAssemblyParallel': 'DOLFIN'})
-    b.plot(xaxis='np', regions=regions)
+    if len(sys.argv) > 1:
+        np = map(int, sys.argv[1:])
+        b = MatrixAssembly(name='DolfinMatrixAssemblyParallel')
+        b.combine_series([('np', np)], filename='DolfinMatrixAssembly')
+        b.save()
+        b = MatrixAssembly(name='FiredrakeMatrixAssemblyParallel')
+        b.combine_series([('np', np)], filename='FiredrakeMatrixAssembly')
+        b.save()
+        b = MatrixAssembly(name='MatrixAssemblyParallel')
+        b.combine({'FiredrakeMatrixAssemblyParallel': 'Firedrake',
+                   'DolfinMatrixAssemblyParallel': 'DOLFIN'})
+        b.plot(xaxis='np', regions=regions)
