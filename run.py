ed3897ea65cb6887152fc639905616e077ae7acc
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-17 12:43:35 +0100

    Allow passing arbitrary benchmark arguments to run script

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ def run(benchmark, np, socket=None, core=0):
         cmd = ['mpirun']
         for p in range(core, n + core):
             pin = 'N:%d' % p if socket is None else 'S%d:%d' % (socket, p)
-            cmd += ['-np', '1', 'likwid-pin', '-c', pin]
-            cmd += ['python', benchmark, ':']
+            cmd += ['-np', '1', 'likwid-pin', '-c', pin, 'python'] + benchmark
+            cmd += [':']
         print datetime.now(), benchmark, 'started'
         print ' '.join(cmd)
         check_call(cmd)
@@ -19,10 +19,11 @@ def run(benchmark, np, socket=None, core=0):
         core += n
 
 if __name__ == '__main__':
-    p = ArgumentParser(description="Run a benchmark")
+    p = ArgumentParser(description="Run a benchmark pinned to CPU cores")
     p.add_argument('--socket', '-s', type=int, help="socket to pin to")
-    p.add_argument('--core', '-c', type=int, default=0, help="inital core to pin to")
-    p.add_argument('benchmark', help="benchmark to run")
-    p.add_argument('np', type=int, nargs='+',
+    p.add_argument('--core', '-c', type=int, default=0,
+                   help="inital core to pin to")
+    p.add_argument('--np', type=int, nargs='+', default=[1],
                    help="number of processes to run")
+    p.add_argument('benchmark', help="benchmark to run", nargs='+')
     run(**vars(p.parse_args()))
