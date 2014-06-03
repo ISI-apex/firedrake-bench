00fc9ca62682f9be7d8e2f5abd3b11ea19976d36
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-06-03 13:07:54 +0100

    Forms: also plot bar chart with log scale

--- forms/forms.py
+++ forms/forms.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
                'DolfinForms_np1': 'DOLFIN'})
     regions = map(' '.join, product(['DOLFIN', 'Firedrake'], regions))
     b.plot(xaxis='degree', regions=regions, xlabel='Polynomial degree',
-           kinds='bar', legend='best')
-    b.plot(xaxis='qdegree', regions=regions, kinds='bar', legend='best',
+           kinds='bar,barlog', legend='best')
+    b.plot(xaxis='qdegree', regions=regions, kinds='bar,barlog', legend='best',
            xlabel='Polynomial degree (premultiplying functions)')
     if len(sys.argv) > 1:
         np = map(int, sys.argv[1:])
