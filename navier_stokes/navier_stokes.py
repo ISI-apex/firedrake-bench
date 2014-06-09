231c4c25db6a59161b39e0be6fdee626485896ab
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-06-09 17:22:42 +0100

    Navier-Stokes: also create plots for only RHS and solve

--- navier_stokes/navier_stokes.py
+++ navier_stokes/navier_stokes.py
@@ -35,15 +35,31 @@ if __name__ == '__main__':
     b.plot(xaxis='scale', regions=regions, xlabel='mesh size (cells)',
            xvalues=b.meta['cells'], kinds='plot,loglog', groups=['variant'],
            title='Navier-Stokes (single core, 2D, P2-P1 discretisation)')
+    for r in r2:
+        b.plot(xaxis='scale', regions=map(' '.join, product(r1, [r])),
+               xlabel='mesh size (cells)', figname='NavierStokes_' + r,
+               xvalues=b.meta['cells'], kinds='plot,loglog', groups=['variant'],
+               title='Navier-Stokes %s (single core, 2D, P2-P1 discretisation)' % r)
     if len(sys.argv) > 1:
+        tsuff = ' (single node, 2D, P2-P1 discretisation, mesh scaling: %(scale)s)'
         np = map(int, sys.argv[1:])
         b = NavierStokes(name='NavierStokesParallel')
         b.combine_series([('np', np), ('variant', ['Firedrake', 'DOLFIN'])],
                          filename='NavierStokes')
         b.plot(xaxis='np', regions=regions, xlabel='Number of processors',
                kinds='plot,loglog', groups=['variant'],
-               title='Navier-Stokes (single node, 2D, P2-P1 discretisation, mesh scaling: %(scale)s)')
+               title='Navier-Stokes' + tsuff)
         b.plot(xaxis='np', regions=regions, xlabel='Number of processors',
                kinds='plot', groups=['variant'], speedup=(1, 'DOLFIN'),
                ylabel='Speedup relative to DOLFIN on 1 core',
-               title='Navier-Stokes (single node, 2D, P2-P1 discretisation, mesh scaling: %(scale)s)')
+               title='Navier-Stokes' + tsuff)
+        for r in r2:
+            b.plot(xaxis='np', regions=map(' '.join, product(r1, [r])),
+                   xlabel='Number of processors', figname='NavierStokesParallel_' + r,
+                   kinds='plot,loglog', groups=['variant'],
+                   title='Navier-Stokes ' + r + tsuff)
+            b.plot(xaxis='np', regions=map(' '.join, product(r1, [r])),
+                   xlabel='Number of processors', figname='NavierStokesParallel_' + r,
+                   kinds='plot', groups=['variant'], speedup=(1, 'DOLFIN'),
+                   ylabel='Speedup relative to DOLFIN on 1 core',
+                   title='Navier-Stokes ' + r + tsuff)
