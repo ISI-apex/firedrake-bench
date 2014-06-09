1fa9e7a90c121809ee08cb8cff11ad6822d76153
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-06-09 16:00:16 +0100

    Forms: customise plot titles

--- forms/forms.py
+++ forms/forms.py
@@ -22,22 +22,28 @@ class Forms(Benchmark):
 if __name__ == '__main__':
     b = Forms()
     b.combine_series([('variant', ['Firedrake', 'DOLFIN'])])
-    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree',
-           kinds='bar,barlog', groups=['variant'])
-    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree',
+    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree q',
+           kinds='bar,barlog', groups=['variant'],
+           title='%(form)s (single core, 3D, premultiplying functions degree %(p)d)')
+    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree q',
            kinds='bar', groups=['variant'], speedup=('DOLFIN',),
-           ylabel='Speedup relative to DOLFIN')
+           ylabel='Speedup relative to DOLFIN',
+           title='%(form)s (single core, 3D, premultiplying functions degree %(p)d)')
     b.plot(xaxis='p', regions=regions, kinds='bar,barlog',
-           xlabel='Polynomial degree (premultiplying functions)',
-           groups=['variant'])
+           xlabel='Polynomial degree (premultiplying functions) p',
+           groups=['variant'],
+           title='%(form)s (single core, 3D, polynomial degree %(q)d)')
     b.plot(xaxis='p', regions=regions, kinds='bar',
-           xlabel='Polynomial degree (premultiplying functions)',
+           xlabel='Polynomial degree (premultiplying functions) p',
            groups=['variant'], speedup=('DOLFIN',),
-           ylabel='Speedup relative to DOLFIN')
-    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree',
+           ylabel='Speedup relative to DOLFIN',
+           title='%(form)s (single core, 3D, polynomial degree %(q)d)')
+    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree q',
            kinds='bar,barlog', groups=['variant', 'p'],
-           legend={'loc': 'best', 'ncol': 2})
-    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree',
+           legend={'loc': 'best', 'ncol': 2},
+           title='%(form)s (single core, 3D)')
+    b.plot(xaxis='q', regions=regions, xlabel='Polynomial degree q',
            kinds='bar', groups=['variant', 'p'], speedup=('DOLFIN',),
            ylabel='Speedup relative to DOLFIN',
-           legend={'loc': 'best', 'ncol': 2})
+           legend={'loc': 'best', 'ncol': 2},
+           title='%(form)s (single core, 3D)')
