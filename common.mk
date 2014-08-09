2ce26dc51e2a1d022a906f7f85a98bd886c0b1f5
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-08-09 20:16:30 +0100

    Add separate make targets for gmsh/xml meshes

--- common.mk
+++ common.mk
@@ -1,7 +1,9 @@
 MSH_MESHES := $(foreach scale, $(SCALES), $(MESH)_$(scale).msh)
 XML_MESHES := $(foreach scale, $(SCALES), $(MESH)_$(scale).xml.gz)
 
-meshes: $(MSH_MESHES) $(XML_MESHES)
+meshes: gmsh xml
+gmsh: $(MSH_MESHES)
+xml: $(XML_MESHES)
 
 $(MESH)_%.msh: $(MESH).geo
 	gmsh -2 -clscale $* -o $@ $<
