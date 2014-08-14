297d5d9a1d7bd9841b718e77833538d15e0a2150
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-08-14 14:12:24 +0100

    Poisson: add parallel efficiency plot for weak scaling

--- poisson/poisson.py
+++ poisson/poisson.py
@@ -48,7 +48,9 @@ if __name__ == '__main__':
                    ylabel='Speedup relative to DOLFIN', speedup=('DOLFIN',),
                    title='Poisson (sequential, 3D)')
     if args.weak:
+        base = args.weak.index(args.base or 1)
         size = args.size[0] if args.size else 1e4
+        efficiency = lambda xvals, yvals: [yvals[base]/y for x, y in zip(xvals, yvals)]
         for degree in degrees:
             dofs = lambda n: (int((size*n)**(1./dim))*degree+1)**dim
             doflabel = lambda n: '%.1fM' % (dofs(n)/1e6) if dofs(n) > 1e6 else '%dk' % (dofs(n)/1e3)
@@ -56,13 +58,18 @@ if __name__ == '__main__':
             b.combine_series([('np', args.weak), ('variant', variants),
                               ('degree', [degree])], filename='Poisson')
             dpp = dofs(args.weak[-1])/(1000*args.weak[-1])
+            xlabel = 'Number of processors / DOFs (DOFs per processor: %dk)' % dpp
+            xticklabels = ['%d\n%s' % (n, doflabel(n)) for n in args.weak]
+            title = 'Poisson (weak scaling, polynomial degree %d, 3D)' % degree
             b.plot(xaxis='np', regions=regions,
-                   xlabel='Number of processors / DOFs (DOFs per processor: %dk)' % dpp,
-                   xticklabels=['%d\n%s' % (n, doflabel(n)) for n in args.weak],
-                   kinds='plot,loglog', groups=['variant'],
-                   title='Poisson (weak scaling, polynomial degree %d, 3D)' % degree)
+                   xlabel=xlabel, xticklabels=xticklabels, kinds='plot,loglog',
+                   groups=['variant'], title=title)
+            b.plot(xaxis='np', regions=regions, figname='PoissonWeakEfficiency',
+                   ylabel='Parallel efficiency w.r.t. %d cores' % args.weak[base],
+                   xlabel=xlabel, xticklabels=xticklabels, kinds='semilogx',
+                   groups=['variant'], title=title, transform=efficiency, ymin=0)
     if args.parallel:
-        base = args.base or 0
+        base = args.parallel.index(args.base or 1)
         efficiency = lambda xvals, yvals: [xvals[base]*yvals[base]/(x*y)
                                            for x, y in zip(xvals, yvals)]
         for degree in degrees:
@@ -78,17 +85,17 @@ if __name__ == '__main__':
                 title = 'Poisson (strong scaling, 3D, polynomial degree %d, %.2fM DOFs)' % (degree, dofs/1e6)
                 xticklabels = ['%d\n%s' % (n, doflabel(n)) for n in args.parallel]
                 xlabel = 'Number of processors / DOFs per processor'
-                b = Poisson(benchmark='PoissonParallel', resultsdir=args.resultsdir, plotdir=args.plotdir)
+                b = Poisson(benchmark='PoissonStrong', resultsdir=args.resultsdir, plotdir=args.plotdir)
                 b.combine_series([('np', args.parallel), ('variant', variants),
                                   ('degree', [degree]), ('size', [size])],
                                  filename='Poisson')
                 b.plot(xaxis='np', regions=regions, xlabel=xlabel,
                        xticklabels=xticklabels, kinds='plot,loglog',
                        groups=['variant'], title=title)
-                b.plot(xaxis='np', regions=regions, figname='PoissonEfficiency',
+                b.plot(xaxis='np', regions=regions, figname='PoissonStrongEfficiency',
                        ylabel='Parallel efficiency w.r.t. %d cores' % args.parallel[base],
                        xlabel=xlabel, xticklabels=xticklabels, kinds='semilogx',
-                       groups=['variant'], title=title, transform=efficiency)
+                       groups=['variant'], title=title, transform=efficiency, ymin=0)
                 if 'DOLFIN' in variants:
                     b.plot(xaxis='np', regions=regions, speedup=(1, 'DOLFIN'),
                            ylabel='Speedup relative to DOLFIN on 1 core',
