d714adad98a6f9518de836c053b26edd7ba55657
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-09-26 14:49:50 +0100

    Navier-Stokes: use parser, add sequential/strong/weak plot

--- navier_stokes/navier_stokes.py
+++ navier_stokes/navier_stokes.py
@@ -1,4 +1,4 @@
-from pybench import Benchmark
+from pybench import Benchmark, parser
 from itertools import product
 
 scale = [1.0, 0.707, 0.5, 0.354, 0.25, 0.177, 0.125]
@@ -62,38 +62,102 @@ class NavierStokes(Benchmark):
     profileregions = ['matrix assembly'] + map(' '.join, product(r1, r2))
 
 if __name__ == '__main__':
-    import sys
-    regions = map(' '.join, product(r1, r2))
-    b = NavierStokes()
-    b.combine_series([('np', [1]), ('scale', scale), ('variant', ['Firedrake', 'DOLFIN'])])
-    b.plot(xaxis='scale', regions=regions, xlabel='mesh size (cells)',
-           xvalues=b.meta['cells'], kinds='plot,loglog', groups=['variant'],
-           title='Navier-Stokes (single core, 2D, P2-P1 discretisation)')
-    for r in r2:
-        b.plot(xaxis='scale', regions=map(' '.join, product(r1, [r])),
-               xlabel='mesh size (cells)', figname='NavierStokes_' + r,
-               xvalues=b.meta['cells'], kinds='plot,loglog', groups=['variant'],
-               title='Navier-Stokes %s (single core, 2D, P2-P1 discretisation)' % r)
-    if len(sys.argv) > 1:
-        tsuff = ' (single node, 2D, P2-P1 discretisation, mesh scaling: %(scale)s)'
-        np = map(int, sys.argv[1:])
-        b = NavierStokes(benchmark='NavierStokesParallel')
-        b.combine_series([('np', np), ('scale', scale), ('variant', ['Firedrake', 'DOLFIN'])],
+    p = parser(description="Plot results for Navier-Stokes benchmark")
+    p.add_argument('-m', '--scale', type=float, nargs='+',
+                   help='mesh scales to plot')
+    p.add_argument('-v', '--variant', nargs='+', help='variants to plot')
+    p.add_argument('-b', '--base', type=int,
+                   help='index of size to use as base for parallel efficiency')
+    p.add_argument('-r', '--region', nargs='+', help='regions to plot')
+    args = p.parse_args()
+    regions = args.region or map(' '.join, product(r1, r2))
+    variants = args.variant or ['Firedrake', 'DOLFIN']
+    groups = ['variant'] if len(variants) > 1 else []
+    scales = args.scale or scale
+    if args.sequential:
+        b = NavierStokes(resultsdir=args.resultsdir, plotdir=args.plotdir)
+        b.combine_series([('np', [1]), ('scale', scales), ('variant', variants)])
+        b.plot(xaxis='scale', regions=regions, xlabel='DOFs', groups=groups,
+               xvalues=[vertices[s] for s in scales], kinds='plot,loglog',
+               title='Navier-Stokes (sequential, 2D, P2-P1)')
+    if args.weak:
+        base = args.weak.index(args.base or 1)
+        scale = args.scale[0] if args.scale else 1.0
+        dofs = lambda n: vertices[round(scale/n**.5, 3)]
+        doflabel = lambda n: '%.1fM' % (dofs(n)/1e6) if dofs(n) > 1e6 else '%dk' % (dofs(n)/1e3)
+        b = NavierStokes(benchmark='NavierStokesWeak', resultsdir=args.resultsdir, plotdir=args.plotdir)
+        b.combine_series([('np', args.weak), ('weak', [scale]), ('variant', variants)],
                          filename='NavierStokes')
-        b.plot(xaxis='np', regions=regions, xlabel='Number of processors',
-               kinds='plot,loglog', groups=['variant'],
-               title='Navier-Stokes' + tsuff)
-        b.plot(xaxis='np', regions=regions, xlabel='Number of processors',
-               kinds='plot', groups=['variant'], speedup=(1, 'DOLFIN'),
-               ylabel='Speedup relative to DOLFIN on 1 core',
-               title='Navier-Stokes' + tsuff)
-        for r in r2:
-            b.plot(xaxis='np', regions=map(' '.join, product(r1, [r])),
-                   xlabel='Number of processors', figname='NavierStokesParallel_' + r,
-                   kinds='plot,loglog', groups=['variant'],
-                   title='Navier-Stokes ' + r + tsuff)
-            b.plot(xaxis='np', regions=map(' '.join, product(r1, [r])),
-                   xlabel='Number of processors', figname='NavierStokesParallel_' + r,
-                   kinds='plot', groups=['variant'], speedup=(1, 'DOLFIN'),
-                   ylabel='Speedup relative to DOLFIN on 1 core',
-                   title='Navier-Stokes ' + r + tsuff)
+        dpp = dofs(args.weak[-1])/(1000*args.weak[-1])
+        xlabel = 'Number of cores / DOFs (DOFs per core: %dk)' % dpp
+        xticklabels = ['%d\n%s' % (n, doflabel(n)) for n in args.weak]
+        title = 'Navier-Stokes (weak scaling, 2D, P2-P1)'
+        if args.base:
+            efficiency = lambda xvals, yvals: [yvals[0]/y for x, y in zip(xvals, yvals)]
+            subplotargs = {(0, 0): {'xvals': args.weak[:base+1],
+                                    'hideyticks': range(2),
+                                    'title': 'intra node: 1-%d cores' % args.base,
+                                    'xticklabels': xticklabels[:base+1],
+                                    'xlabel': None},
+                           (0, 1): {'xvals': args.weak[base:],
+                                    'title': 'inter node: %d-%d cores' % (args.base, args.weak[-1]),
+                                    'xticklabels': xticklabels[base:],
+                                    'xlabel': None,
+                                    'ylabel': None},
+                           (1, 0): {'xvals': args.weak[:base+1],
+                                    'xticklabels': xticklabels[:base+1],
+                                    'transform': efficiency,
+                                    'ymin': 0,
+                                    'xlabel': 'Number of cores / DOFs',
+                                    'ylabel': 'Parallel efficiency w.r.t. 1/%d cores' % args.weak[base]},
+                           (1, 1): {'xvals': args.weak[base:],
+                                    'xticklabels': xticklabels[base:],
+                                    'transform': efficiency,
+                                    'hidexticks': range(2),
+                                    'ymin': 0,
+                                    'xlabel': 'DOFs per core: %dk' % dpp,
+                                    'ylabel': None}}
+            b.plot(xaxis='np', regions=regions, kinds='plot', groups=groups,
+                   title=title, subplots=(2, 2), sharex='col', sharey='row',
+                   hspace=0.02, wspace=0.02, subplotargs=subplotargs)
+        else:
+            efficiency = lambda xvals, yvals: [yvals[base]/y for x, y in zip(xvals, yvals)]
+            b.plot(xaxis='np', regions=regions, hidexticks=range(5),
+                   xlabel=xlabel, xticklabels=xticklabels, kinds='plot,loglog',
+                   groups=groups, title=title)
+            b.plot(xaxis='np', regions=regions, hidexticks=range(5),
+                   figname='NavierStokesWeakEfficiency',
+                   ylabel='Parallel efficiency w.r.t. %d cores' % args.weak[base],
+                   xlabel=xlabel, xticklabels=xticklabels, kinds='plot',
+                   groups=groups, title=title, transform=efficiency, ymin=0)
+    if args.parallel:
+        base = args.parallel.index(args.base or 1)
+        efficiency = lambda xvals, yvals: [xvals[base]*yvals[base]/(x*y)
+                                           for x, y in zip(xvals, yvals)]
+        for sc in scales:
+            dofs = vertices[sc]
+
+            def doflabel(n):
+                if dofs > 1e6*n:
+                    return '%.2fM' % (dofs/(1e6*n))
+                elif dofs > 1e3*n:
+                    return '%dk' % (dofs/(1e3*n))
+                return str(dofs/n)
+            title = 'Navier-Stokes (strong scaling, 2D, %.2fM cells, %.2fM DOFs)' % (cells[sc]/1e6, dofs/1e6)
+            xticklabels = ['%d\n%s' % (n, doflabel(n)) for n in args.parallel]
+            xlabel = 'Number of cores / DOFs per core'
+            b = NavierStokes(benchmark='NavierStokesStrong', resultsdir=args.resultsdir, plotdir=args.plotdir)
+            b.combine_series([('np', args.parallel), ('scale', [sc]),
+                              ('variant', variants)], filename='NavierStokes')
+            b.plot(xaxis='np', regions=regions, xlabel=xlabel,
+                   xticklabels=xticklabels, kinds='loglog',
+                   groups=groups, title=title, trendline='perfect speedup')
+            b.plot(xaxis='np', regions=regions, figname='NavierStokesStrongEfficiency',
+                   ylabel='Parallel efficiency w.r.t. %d cores' % args.parallel[base],
+                   xlabel=xlabel, xticklabels=xticklabels, kinds='plot',
+                   groups=groups, title=title, transform=efficiency, ymin=0)
+            if 'DOLFIN' in variants:
+                b.plot(xaxis='np', regions=regions, speedup=(1, 'DOLFIN'),
+                       ylabel='Speedup relative to DOLFIN on 1 core',
+                       xlabel=xlabel, xticklabels=xticklabels, kinds='plot',
+                       groups=groups, title=title)
