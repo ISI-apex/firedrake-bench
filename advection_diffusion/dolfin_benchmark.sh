1f01f452a9abc311e7ecd2aa2f39f560e35d8c48
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-10-09 22:06:13 +0100

    Advection-diffusion: remove outdated benchmarks scripts

--- advection_diffusion/dolfin_benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for np in 1 2 4
-do
-  echo Benchmarking DOLFIN on $np processes
-  echo ==================================
-  echo
-  mpirun -np $np python dolfin_advection_diffusion.py
-  echo
-done
--- advection_diffusion/firedrake_benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for np in 1 2 4
-do
-  echo Benchmarking Firedrake on $np processes
-  echo =====================================
-  echo
-  mpirun -np $np python firedrake_advection_diffusion.py
-  echo
-done
