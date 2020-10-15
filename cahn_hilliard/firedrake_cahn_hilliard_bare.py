f6d990dfee4368e1504c15153195aaa32c4fde59
Alexei Colin <acolin@isi.edu>
2020-10-15 21:28:05 +0000

    cahn_hilliard: argparser for bare wrapper

--- cahn_hilliard/firedrake_cahn_hilliard_bare.py
+++ cahn_hilliard/firedrake_cahn_hilliard_bare.py
@@ -1,64 +1,81 @@
 import sys
 import time
+import argparse
 
 from mpi4py import MPI
 from firedrake import *
 
 from firedrake_cahn_hilliard_problem import CahnHilliardProblem
 
-size = int(sys.argv[1])
-fout_name = sys.argv[2]
-
-steps=1
-degree=1
-pc='fieldsplit'
-inner_ksp='preonly'
-ksp='gmres'
-maxit=1
-save=False
-compute_norms=True
-verbose=False
+parser = argparse.ArgumentParser(
+        description="Invoke Cahn-Hilliard CFD problem")
+parser.add_argument("mesh_size", type=int,
+        help="Size of the unit square mesh along one dimension")
+parser.add_argument("--solution-out",
+        help="Output filename where to save solution (PVD)")
+parser.add_argument("--elapsed-out",
+        help="Output filename where to save measured times (CSV)")
+parser.add_argument("--degree", type=int, default=1,
+        help="Degree of the problem")
+parser.add_argument("--steps", type=int, default=1,
+        help="Number of timesteps to solve")
+parser.add_argument("--preconditioner", default='fieldsplit',
+        help="Preconditioner to use")
+parser.add_argument("--ksp", default='gmres',
+        help="Solver to use")
+parser.add_argument("--inner-ksp", default='preonly',
+        help="Inner solver to use")
+parser.add_argument("--max-iterations", type=int, default=1,
+        help="Inner solver to use")
+parser.add_argument("--lmbda", type=float, default=1.0e-02,
+        help="Surface parameter")
+parser.add_argument("--dt", type=float, default=5.0e-06,
+        help="Time step")
+parser.add_argument("--theta", type=float, default=0.5,
+        help="Time stepping family, e.g. theta=1 -> backward Euler, theta=0.5 -> Crank-Nicolson")
+parser.add_argument("--compute-norms", action='store_true',
+        help="Compute and print norms")
+parser.add_argument("--verbose", action='store_true',
+        help="Enable extra logging")
+args = parser.parse_args()
 
 comm = MPI.COMM_WORLD
 
-# Model parameters
-lmbda = 1.0e-02  # surface parameter
-dt = 5.0e-06     # time step
-theta = 0.5      # time stepping family, e.g. theta=1 -> backward Euler, theta=0.5 -> Crank-Nicolson
-
-params = CahnHilliardProblem.get_solve_params(verbose=verbose, \
-        pc=pc, ksp=ksp, inner_ksp=inner_ksp, maxit=maxit)
+params = CahnHilliardProblem.get_solve_params(
+        pc=args.preconditioner, ksp=args.ksp, inner_ksp=args.inner_ksp,
+        maxit=args.max_iterations, verbose=args.verbose)
 
 time_mesh_begin = time.time()
-mesh = CahnHilliardProblem.make_mesh(size)
+mesh = CahnHilliardProblem.make_mesh(args.mesh_size)
 time_mesh_end = time.time()
 
 time_setup_begin = time.time()
-u, u0, solver = CahnHilliardProblem.do_setup(mesh, pc, degree=degree,
-        dt=dt, theta=theta, lmbda=lmbda, params=params)
+u, u0, solver = CahnHilliardProblem.do_setup(mesh, args.preconditioner,
+        degree=args.degree, dt=args.dt, theta=args.theta,
+        lmbda=args.lmbda, params=params)
 time_setup_end = time.time()
 
 # Output file
-if save:
-    file = File("vtk/firedrake_cahn_hilliard_%d.pvd" % size)
+if args.solution_out is not None:
+    file = File(args.solution_out)
 else:
     file = None
 
 time_solve_begin = time.time()
-CahnHilliardProblem.do_solve(u, u0, solver, steps,
-        compute_norms=compute_norms, out_file=file)
+CahnHilliardProblem.do_solve(u, u0, solver, args.steps,
+        compute_norms=args.compute_norms, out_file=file)
 time_solve_end = time.time()
 
 if comm.rank == 0:
     from collections import OrderedDict
     times = OrderedDict()
-    times["mesh"] = size
+    times["mesh"] = args.mesh_size
     times["mesh_s"]  = time_mesh_end - time_mesh_begin
     times["setup_s"] = time_setup_end - time_setup_begin
     times["solve_s"] = time_solve_end - time_solve_begin
 
     # note: if you open this earlier, the FD breaks somehow (???)
-    fout = open(fout_name, "w")
+    fout = open(args.elapsed_out, "w")
     print(",".join(times.keys()), file=fout)
     print(",".join([str(v) for v in times.values()]), file=fout)
     fout.close()
